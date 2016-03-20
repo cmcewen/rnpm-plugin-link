4050abb8d2e485dd53aeb329c167bdde29b9751d
Mike Grabowski <grabbou@gmail.com>
2016-03-20 11:53:25 +0100

    Styling

--- src/groupBy.js
+++ src/groupBy.js
@@ -2,18 +2,15 @@
  * Returns items grouped by the key
  */
 module.exports = function groupBy(items, getItemKey) {
-  return items.reduce(
-    (acc, item) => {
-      const key = getItemKey(item);
+  return items.reduce((acc, item) => {
+    const key = getItemKey(item);
 
-      if (!acc[key]) {
-        acc[key] = [item];
-      } else {
-        acc[key] = acc[key].concat(item);
-      }
+    if (!acc[key]) {
+      acc[key] = [item];
+    } else {
+      acc[key] = acc[key].concat(item);
+    }
 
-      return acc;
-    },
-    {}
-  );
+    return acc;
+  }, {});
 };
